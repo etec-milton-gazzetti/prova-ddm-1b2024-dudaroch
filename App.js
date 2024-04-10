6379da255f1d7998de0c08d35d89c76f24c238b6
dudaroch <134696629+dudaroch@users.noreply.github.com>
2024-04-10 17:44:53 +0000

    [feat] botão

--- App.js
+++ App.js
@@ -16,8 +16,10 @@ import politica from "./assets/economia.png"
 import cultura from "./assets/sociedade.png"
 import fundosobre from "./assets/sobre.png"
 import minhafoto from "./assets/minhafoto.png"
-
-
+import deusesgregos from "./assets/deusesgregos.png"
+import mitologia from "./assets/mitologia.png"
+import obras from "./assets/obras3.png"
+import obrasescrita from "./assets/obrasescrita.png"
 
 
 
@@ -180,21 +182,22 @@ function MythologyScreen({navigation}) {
       <Image source={greciafundo}
       style={{width:500, height: 50, }}
       />
-      <Image source={escrita} style={{ paddingTop: 35, width: 300, height: 30, marginTop: 50, alignSelf: 'center'}} />
-      <Image source={arquitetura} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
+      <Image source={mitologia} style={{ paddingTop: 35, width: 300, height: 60, marginTop: 50, alignSelf: 'center'}} />
+      <Image source={deusesgregos} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
 
       <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 15, fontFamily: 'OpenSans_400Regular'}}>
-        Grécia Antiga é o nome dado à civilização que foi constituída na área que abrange o sul da península Balcânica, ilhas do mar Egeu e litoral da Ásia Menor. A importância da Grécia Antiga para o mundo ocidental é grande em virtude, principalmente, dos aspectos cultural, científico, filosófico e político dessa civilização que foram legados à civilização contemporânea.
+      Os deuses do Olimpo são: Zeus, Hera, Poseidon, Atena, Ares, Deméter, Apolo, Ártemis, Hefesto, Afrodite, Hermes e Dionísio. Eles viviam no monte Olimpo, o monte mais alto de toda Grécia. Ficavam no topo, acima das nuvens. A mitologia grega diz que o portão era de nuvens, inclusive.
       </Text>
-      <Image source={politica} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
-      <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 15, fontFamily: 'OpenSans_400Regular'}}>
-      Na Grécia Antiga, a economia era baseada na agricultura, especialmente na produção de olivas, uvas e cereais, além do comércio marítimo impulsionado pela localização favorável da região no Mediterrâneo. Atenas e Esparta tinham sistemas econômicos distintos, com Atenas focada no comércio e Esparta na agricultura e controle territorial.
-      Politicamente, a Grécia Antiga foi marcada pelo desenvolvimento da democracia em Atenas, onde os cidadãos livres tinham direitos políticos, embora fossem restritos a uma parcela da população. Esparta, em contraste, era governada por uma oligarquia militar, com ênfase na disciplina e no treinamento militar.
+
+      <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 10, fontFamily: 'OpenSans_400Regular'}}>
+      Os deuses tinham grande importância para os gregos, possuindo templos em sua homenagem, e muitos deles se tornavam patronos de cidades inteiras. A cidade de Atenas, por exemplo, tinha em Atena sua patrona, enquanto Corinto tinha em Afrodite a sua deusa patrona.
       </Text>
-      <Image source={cultura} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
-      <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 15, fontFamily: 'OpenSans_400Regular'}}>
-      Na sociedade grega antiga, havia uma clara hierarquia social, onde os cidadãos livres, geralmente proprietários de terras ou comerciantes, ocupavam o topo e desfrutavam de direitos políticos. Abaixo deles estavam os metecos, estrangeiros residentes, e os escravos, que não tinham direitos e eram considerados propriedade de seus senhores. As mulheres ocupavam uma posição subordinada, com direitos limitados e responsabilidades principalmente no espaço doméstico.
-      A cultura grega antiga é amplamente admirada e influente. Sua literatura, filosofia, arte, arquitetura e ciência deixaram um legado duradouro. As epopeias de Homero, as obras filosóficas de Sócrates, Platão e Aristóteles, e o teatro de dramaturgos como Sófocles e Eurípedes são exemplos notáveis de suas contribuições culturais. A arquitetura grega, com seus templos e colunas, continua a inspirar projetos arquitetônicos modernos. Além disso, a Grécia antiga foi pioneira em campos científicos como matemática, astronomia e medicina.
+
+
+      <Image source={obrasescrita} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
+      <Image source={obras} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
+      <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 0, fontFamily: 'OpenSans_400Regular'}}>
+      Essas são algumas obras que foram inspiradas na mitologia grega.
       </Text>
 
       <TouchableOpacity onPress={()=>navigation.goBack()} style={{
