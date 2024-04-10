d33adb78bccc44bee3a3db11b48844df60aa6938
dudaroch <134696629+dudaroch@users.noreply.github.com>
2024-04-10 12:22:11 +0000

    [feat] página mitologia

--- App.js
+++ App.js
@@ -47,7 +47,7 @@ function HomeScreen({navigation}) {
         <Text style={{ fontFamily: 'Cinzel_400Regular', fontSize: 25, color: '#F0EFEC' }}> História </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={()=>navigation.navigate("About")} style={{
+      <TouchableOpacity onPress={()=>navigation.navigate("Mitologia")} style={{
         backgroundColor: '#264B85',
         width: 250,
         height: 50,
@@ -172,6 +172,50 @@ function HistoryScreen({navigation}) {
   );
 }
 
+function MythologyScreen({navigation}) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      
+      <Image source={greciafundo}
+      style={{width:500, height: 50, }}
+      />
+      <Image source={escrita} style={{ paddingTop: 35, width: 300, height: 30, marginTop: 50, alignSelf: 'center'}} />
+      <Image source={arquitetura} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
+
+      <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 15, fontFamily: 'OpenSans_400Regular'}}>
+        Grécia Antiga é o nome dado à civilização que foi constituída na área que abrange o sul da península Balcânica, ilhas do mar Egeu e litoral da Ásia Menor. A importância da Grécia Antiga para o mundo ocidental é grande em virtude, principalmente, dos aspectos cultural, científico, filosófico e político dessa civilização que foram legados à civilização contemporânea.
+      </Text>
+      <Image source={politica} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
+      <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 15, fontFamily: 'OpenSans_400Regular'}}>
+      Na Grécia Antiga, a economia era baseada na agricultura, especialmente na produção de olivas, uvas e cereais, além do comércio marítimo impulsionado pela localização favorável da região no Mediterrâneo. Atenas e Esparta tinham sistemas econômicos distintos, com Atenas focada no comércio e Esparta na agricultura e controle territorial.
+      Politicamente, a Grécia Antiga foi marcada pelo desenvolvimento da democracia em Atenas, onde os cidadãos livres tinham direitos políticos, embora fossem restritos a uma parcela da população. Esparta, em contraste, era governada por uma oligarquia militar, com ênfase na disciplina e no treinamento militar.
+      </Text>
+      <Image source={cultura} style={{width: 350, height: 200, marginTop: 30, alignSelf: 'center'}} />
+      <Text style={{marginLeft:40, marginRight: 40, textAlign: 'justify', marginTop: 15, fontFamily: 'OpenSans_400Regular'}}>
+      Na sociedade grega antiga, havia uma clara hierarquia social, onde os cidadãos livres, geralmente proprietários de terras ou comerciantes, ocupavam o topo e desfrutavam de direitos políticos. Abaixo deles estavam os metecos, estrangeiros residentes, e os escravos, que não tinham direitos e eram considerados propriedade de seus senhores. As mulheres ocupavam uma posição subordinada, com direitos limitados e responsabilidades principalmente no espaço doméstico.
+      A cultura grega antiga é amplamente admirada e influente. Sua literatura, filosofia, arte, arquitetura e ciência deixaram um legado duradouro. As epopeias de Homero, as obras filosóficas de Sócrates, Platão e Aristóteles, e o teatro de dramaturgos como Sófocles e Eurípedes são exemplos notáveis de suas contribuições culturais. A arquitetura grega, com seus templos e colunas, continua a inspirar projetos arquitetônicos modernos. Além disso, a Grécia antiga foi pioneira em campos científicos como matemática, astronomia e medicina.
+      </Text>
+
+      <TouchableOpacity onPress={()=>navigation.goBack()} style={{
+        backgroundColor: '#264B85',
+        width: 250,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        alignSelf: 'center',
+        marginTop: 50,}}> 
+      <Text style={{ fontFamily: 'Cinzel_400Regular', fontSize: 25, color: '#F0EFEC' }}>
+        Voltar
+      </Text>
+      </TouchableOpacity>
+      </ScrollView>
+      
+    </SafeAreaView>
+  );
+}
+
 
 const Stack = createNativeStackNavigator();
 
@@ -197,6 +241,7 @@ export default function App() {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="About" component={AboutScreen} />
       <Stack.Screen name="História" component={HistoryScreen} />
+      <Stack.Screen name="Mitologia" component={MythologyScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
